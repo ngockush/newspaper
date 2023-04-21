d381140f9abe5088df6966ca3289335da65fcfdc
NgocKush <44048416+ngockush@users.noreply.github.com>
2023-04-21 19:10:52 +0700

    PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [{
+        name: "ngockush",
+        script: "app.js",
+        watch: true,
+        env: {
+            NODE_ENV: "production",
+        },
+    }, ],
+};
